acc9aa8e631e78c1b4129449179b70f771ef90b6
G2CODE33 <g2code33@gmail.com>
2026-07-10 13:55:33 +0000

    coded restore

--- src/pages/SlideReader.tsx
+++ src/pages/SlideReader.tsx
@@ -413,7 +413,6 @@ const SlideReader: React.FC = () => {
             <div ref={browserContainerRef} className="flex-1 overflow-hidden bg-[#1E293B] relative flex items-center justify-center">
               <Loader2 className="w-8 h-8 text-gray-500 animate-spin" />
             </div>
-          </div>
         )}
       </div>
     </div>
